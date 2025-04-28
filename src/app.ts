244dc15ff2a80aa5b2b0261369c3264307696e30
FazleRahmanEvan <evanshakib26@gmail.com>
2025-04-28 13:27:14 +0600

    app updated

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db";
 import authRoutes from "./routes/authRoutes";
 import clientRoutes from "./routes/clientRoutes";
 import dashboardRoutes from "./routes/dashboardRoutes";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/clients", clientRoutes);
+app.use("/api/projects", projectRoutes);
 
 // Example route (you can remove this later)
 app.get("/", (_req, res) => {
