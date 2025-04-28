b20e77b43c8cd30ff62cb149debc2ec7f0434183
FazleRahmanEvan <evanshakib26@gmail.com>
2025-04-28 13:53:18 +0600

    error handle

---
+++ src/services/authService.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User, { IUser } from "../models/User";
+import { JWT_SECRET } from "../config/env";
+
+interface IUserWithPassword extends IUser {
+  password: string;
+}
+
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error("User already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  await user.save();
+  return generateToken(user);
+};
+
+export const loginUser = async (email: string, password: string) => {
+  const user = (await User.findOne({ email }).select(
+    "+password"
+  )) as IUserWithPassword | null;
+
+  if (!user || !user.password) {
+    throw new Error("Invalid credentials");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  return generateToken(user);
+};
+
+const generateToken = (user: IUser) => {
+  return jwt.sign({ id: user._id }, JWT_SECRET as string, {
+    expiresIn: "30d",
+  });
+};
